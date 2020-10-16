61e50177cd606b78210de63149c503a92edeeb49
asari-matsuda <asari-matsuda@users.noreply.github.com>
2020-10-16 14:38:35 +0900

    commit

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -14,21 +14,17 @@ function check() {
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;
+        }
         const item = XHR.response.post;
         if (item.checked === true) {
           post.setAttribute("data-check", "true");
         } else if (item.checked === false) {
           post.removeAttribute("data-check");
         }
-        if (XHR.status != 200) {
-          alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        } else {
-          return null;
-        }
       }
-      XHR.onerror = function () {
-        alert("Request failed");
-      };
     });
   });
 }
